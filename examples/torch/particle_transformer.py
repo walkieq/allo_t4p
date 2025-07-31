f68c0e999f8a0c464cd8f23ab78cb110b24caddd
ZRQ0410 <1292732905@qq.com>
2025-07-31 02:10:32 +0100

    Implement functions unsupported in allo

--- examples/torch/particle_transformer.py
+++ examples/torch/particle_transformer.py
@@ -1,10 +1,13 @@
 import allo
 import torch
-import math
 import numpy as np
-import torch.nn as nn
-import torch.nn.functional as F
-from model import ConstituentNet
+import os
+from model import ConstituentNet, SliceFirstDim
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+allo_t4p_dir = os.path.dirname(os.path.dirname(cur_dir))
+llvm_build_dir = os.path.join(allo_t4p_dir, "externals/llvm-project/build")
+os.environ["LLVM_BUILD_DIR"] = llvm_build_dir
 
 num_particles = 8
 num_feats = 3
@@ -26,21 +29,42 @@ model_config = {
     "normalization": "Batch",
 }
 
-module = ConstituentNet(**model_config).eval()
+model = ConstituentNet(**model_config).eval()
 example_inputs = [torch.rand(batch_size, num_particles, num_feats)]
-golden = module(*example_inputs)
-llvm_mod = allo.frontend.from_pytorch(
-    module,
-    example_inputs=example_inputs,
-    verbose=True,
-)
+golden = model(*example_inputs)
+np_input = example_inputs[0].detach().numpy()
 
-golden = module(*example_inputs)
-np_inputs = [x.detach().numpy() for x in example_inputs]
-res = llvm_mod(*np_inputs)
-np.testing.assert_allclose(res, golden.detach().numpy(), atol=1e-3)
-print("Test passed!")
+# LLVM
+# llvm_mod = allo.frontend.from_pytorch(
+#     model,
+#     example_inputs=example_inputs,
+#     leaf_modules=(SliceFirstDim,),
+#     verbose=False,
+# )
+# res = llvm_mod(np_input)
+# np.testing.assert_allclose(res, golden.detach().numpy(), atol=1e-5)
+# print("Test passed!")
 
 # generate HLS module
 # mod = allo.frontend.from_pytorch(module, example_inputs=example_inputs, target="vhls")
 # print(mod.hls_code)
+
+# VITIS HLS
+mode = "sw_emu"
+os.environ["XDEVICE"] = "xilinx_u250_gen3x16_xdma_4_1_202210_1"
+os.environ["XCL_EMULATION_MODE"] = mode
+
+vitis_mod = allo.frontend.from_pytorch(
+    model,
+    example_inputs=example_inputs,
+    leaf_modules=(SliceFirstDim,),
+    target="vitis_hls",
+    mode=mode,
+    project="transformer_sw.prj",
+    verbose=False,
+)
+
+allo_out = np.zeros((batch_size, 5), dtype=np.float32)
+vitis_mod(np_input, allo_out)
+np.testing.assert_allclose(allo_out, golden.detach().numpy(), rtol=1e-5, atol=1e-5)
+print("Passed!")
